dcc0b696887154dc423d75efa25ca01a5935a92b
liujinghan <jinghan.liu@yunjiaplus.com>
2019-10-31 14:27:09 +0800

    new branch

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 # 1. sh ./build.sh
 # 1.1 所有需要壳的路径[]
 # 1.2 git 的执行指令
-git remote set-url feature/wrapper  git@git.yunjia.com:h5_web_grp/yun_manage.git
+git remote set-url feature/wrapper git@git.yunjia.com:h5_web_grp/yun_manage.git
 git push feature/wrapper declaredfile:feature/wrapper
