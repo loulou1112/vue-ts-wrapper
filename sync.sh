133241b1862172eb9ec031119b7daae5e448bf35
liujinghan <jinghan.liu@yunjiaplus.com>
2019-10-31 15:05:32 +0800

    finish sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 已经 commit 或者 合并线上后， CI
-# 1. sh ./build.sh
+# 1. sh ./sync.sh
 # 1.1 所有需要壳的路径[]
 # 1.2 git 的执行指令
 git remote set-url feature/wrapper git@git.yunjia.com:h5_web_grp/yun_manage.git
