4fb519f96e7527728fe8ac4ab4cf6914e4b66c55
liujinghan <jinghan.liu@yunjiaplus.com>
2019-11-01 11:42:01 +0800

    finish sync shell

--- sync.sh
+++ sync.sh
@@ -4,5 +4,9 @@
 # 1. sh ./sync.sh
 # 1.1 所有需要壳的路径[]
 # 1.2 git 的执行指令
-git remote set-url feature/wrapper git@git.yunjia.com:h5_web_grp/yun_manage.git
-git push feature/wrapper declaredfile:feature/wrapper
+addresses=('git@git.yunjia.com:h5_web_grp/yun_manage.git' 'git@git.yunjia.com:h5_web_grp/h5_product.git')
+for value in ${addresses[@]};
+do
+  git remote set-url feature/wrapper $value
+  git push feature/wrapper feature/wrapper:feature/wrapper
+done
